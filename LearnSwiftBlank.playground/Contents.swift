829b39982ed3ad07c25336a016077f79509bcc62
chaomeng <custom_mail@163.com>
2021-11-25 23:12:29 +0800

    代码提交

--- LearnSwiftBlank.playground/Contents.swift
+++ LearnSwiftBlank.playground/Contents.swift
@@ -1,9 +1,10 @@
 import UIKit
 
+// TODO: 第一章 -------------
+// TODO: 第一章 -------------
 var greeting = "Hello, playground"
 greeting += "!"
-
-print(greeting)
+print("第一章：" + greeting)
 
 // TODO: 第二章 -------------
 // 类型、常量和变量
